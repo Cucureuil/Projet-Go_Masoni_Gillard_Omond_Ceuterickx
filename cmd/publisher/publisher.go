72a69e9fd0d2a3ea508439474c019da93b9d56a1
cucureuil <chloe.masoni@imt-atlantique.net>
2019-10-16 13:47:41 +0200

    publisher : envoie une structure JSON

--- cmd/publisher/publisher.go
+++ cmd/publisher/publisher.go
@@ -3,10 +3,55 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"time"
+	"encoding/json"
+	mqtt "github.com/eclipse/paho.mqtt.golang"
 	"Projet-Go_Masoni_Gillard_Omond_Ceuterickx/intern/entities/sensors"
 )
 
+
 func main() {
 	s1 := sensors.NewSensorWind(1,"ID","Wind", 1.0,"2019")
-	fmt.Println(s1)
+
+	//msg := "HELLO SUB"
+	msg, err := json.Marshal(s1)
+    if err != nil {
+        fmt.Println(err)
+        return
+	}
+	fmt.Println(msg)
+	topic := "test"
+
+	client_pub := connect("tcp://localhost:1883", "clientPub")
+	client_pub.Publish(topic, 0, false, msg).Wait()
+
+	time.Sleep(100 * time.Second)
+	client_pub.Disconnect(10000)
 }
+
+func createClientOptions(brokerURI string, clientID string) *mqtt.ClientOptions {
+	opts := mqtt.NewClientOptions()
+	// AddBroker adds a broker URI to the list of brokers to be used.
+	// The format should be "scheme://host:port"
+	opts.AddBroker(brokerURI)
+	// opts.SetUsername (user)
+	// opts.SetPassword (password)
+	opts.SetClientID(clientID)
+	return opts
+}
+
+func connect(brokerURI string, clientID string) mqtt.Client {
+	fmt.Println("Trying to connect (" + brokerURI + ", " + clientID + ")...)")
+	opts := createClientOptions(brokerURI, clientID)
+	client := mqtt.NewClient(opts)
+	token := client.Connect()
+	for !token.WaitTimeout(3 * time.Second) {
+	}
+	if err := token.Error(); err != nil {
+		log.Fatal(err)
+	}
+	return client
+}
+
+
--- intern/entities/sensors/Sensor.go
+++ intern/entities/sensors/Sensor.go
@@ -1,14 +1,15 @@
 package sensors
 
+// attributs with UpperCase because of json 
 type sensor struct {
-    idSensor int
-	idAirport  string // only 3 char
-    typeMeasure string // Temperature, Atmospheric pressure, Wind speed
-    value float64
-    dateMeasure string // (timestamp : YYYY-MM-DD-hh-mm-ss)
+    IdSensor int
+	IdAirport  string // only 3 char
+    TypeMeasure string // Temperature, Atmospheric pressure, Wind speed
+    Value float64
+    DateMeasure string // (timestamp : YYYY-MM-DD-hh-mm-ss)
 }
 
 func NewSensorWind(idSensor int,idAirport string,typeMeasure string,value float64, dateMeasure string ) *sensor {
-    sensor := sensor{idSensor: idSensor, idAirport:idAirport,typeMeasure:typeMeasure,value:value, dateMeasure:dateMeasure  }
+    sensor := sensor{IdSensor: idSensor, IdAirport:idAirport,TypeMeasure:typeMeasure,Value:value, DateMeasure:dateMeasure  }
     return &sensor
 }
