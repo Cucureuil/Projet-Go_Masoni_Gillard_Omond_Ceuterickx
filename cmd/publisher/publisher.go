0a5a5fd538db19da1a530f66580127d8381eb66a
Lucille Gillard <lucille.gillard@macroprog.com>
2019-10-16 12:22:24 +0200

    Upd: publish with configs infos

--- cmd/publisher/publisher.go
+++ cmd/publisher/publisher.go
@@ -6,11 +6,13 @@ import (
 	"log"
 	"time"
 
+	"Projet-Go_Masoni_Gillard_Omond_Ceuterickx/config"
+
 	mqtt "github.com/eclipse/paho.mqtt.golang"
 )
 
 func main() {
-	Publish()
+	Publish(config.GetPress(), "test", "Pressure = 111")
 }
 
 func createClientOptions(brokerURI string, clientId string) *mqtt.ClientOptions {
@@ -39,8 +41,9 @@ func connect(brokerURI string, clientId string) mqtt.Client {
 	return client
 }
 
-func Publish() {
-	client2 := connect("tcp://localhost:8080", "ID002")
-	client2.Publish("test", 0, false, "coucou")
+func Publish(conf *config.Config, topic string, msg string) {
+	client2 := connect(conf.BrokerAddress+":"+conf.BrokerPort, conf.ClientID)
+	client2.Publish(topic, 0, false, msg)
 	time.Sleep(5 * time.Second)
+	fmt.Println("Message sended by " + conf.ClientID)
 }
