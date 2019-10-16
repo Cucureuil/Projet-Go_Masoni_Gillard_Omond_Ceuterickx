627cada0526f3f9ea04588d98bfba14f116a883c
Lucille Gillard <lucille.gillard@macroprog.com>
2019-10-16 11:08:35 +0200

    Add: publisher simple version

--- cmd/publisher/publisher.go
+++ cmd/publisher/publisher.go
@@ -3,8 +3,44 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"time"
+
+	mqtt "github.com/eclipse/paho.mqtt.golang"
 )
 
 func main() {
-	fmt.Println("Hello World!")
+	Publish()
+}
+
+func createClientOptions(brokerURI string, clientId string) *mqtt.ClientOptions {
+	opts := mqtt.NewClientOptions()
+
+	// AddBroker adds a broker URI to the list of brokers to be used.
+	// The format should be "scheme://host:port"
+	opts.AddBroker(brokerURI)
+
+	// opts.SetUsername(user)
+	// opts.SetPassword(password)
+	opts.SetClientID(clientId)
+	return opts
+}
+
+func connect(brokerURI string, clientId string) mqtt.Client {
+	fmt.Println("Trying to connect (" + brokerURI + ", " + clientId + ") ...")
+	opts := createClientOptions(brokerURI, clientId)
+	client := mqtt.NewClient(opts)
+	token := client.Connect()
+	for !token.WaitTimeout(3 * time.Second) {
+	}
+	if err := token.Error(); err != nil {
+		log.Fatal(err)
+	}
+	return client
+}
+
+func Publish() {
+	client2 := connect("tcp://localhost:8080", "ID002")
+	client2.Publish("test", 0, false, "coucou")
+	time.Sleep(5 * time.Second)
 }
