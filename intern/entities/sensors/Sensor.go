6744f6836056071fe7f2845bc616108372b1b464
cucureuil <chloe.masoni@imt-atlantique.net>
2019-10-16 15:41:51 +0200

    fix date et valeurs de mesures. Simulation d'envoie de donnees opérationnel (a fixer: generation d'id d'aeroport)

--- intern/entities/sensors/Sensor.go
+++ intern/entities/sensors/Sensor.go
@@ -3,6 +3,8 @@ package sensors
 import(
     "fmt"
     "math/rand"
+    "time"
+    "math"
 )
 // attributs with UpperCase because of json 
 type sensor struct {
@@ -29,15 +31,44 @@ func NewSensorPressure(idSensor int,idAirport string,value float64, dateMeasure 
 }
 
 func RandSensor() *sensor {
-	i:=rand.Intn(30)
+    i:=rand.Intn(30)
+    date := getDate()
+
 	fmt.Println(i)
 	switch {
-		case i<=10:
-			return NewSensorWind(1,"ID", 1.0,"2019")
-		case i<=20 && i >10:
-			return NewSensorTemperature(2,"ID", 1.0,"2019")
-		case i<=30 && i>20:
-			return NewSensorPressure(3,"ID", 1.0,"2019")
+        case i<=10:
+            wind := roundMeasureValue(randWindSpeed())
+			return NewSensorWind(1,"ID", wind,date)
+        case i<=20 && i >10:
+            temp := roundMeasureValue(randTemperature())
+			return NewSensorTemperature(2,"ID", temp,date)
+        case i<=30 && i>20:
+            pres := roundMeasureValue(randPressure())
+			return NewSensorPressure(3,"ID", pres,date)
     }
     return nil
+}
+
+func roundMeasureValue(val float64) float64{
+    return math.Round(val*100)/100
+}
+
+// km/h
+func randWindSpeed() float64{
+    return 0 + rand.Float64() * (130 - 0)
+}
+
+// ° C
+func randTemperature() float64{
+    return -30 + rand.Float64() * (50 - (-30))
+}
+
+// bar
+func randPressure() float64{
+    return 0 + rand.Float64() * (2000 - 0)
+}
+
+// YYYY-MM-DD-hh-mm-ss
+func getDate() string{
+    return time.Now().Format("2006-01-02-15-04-05")     
 }
