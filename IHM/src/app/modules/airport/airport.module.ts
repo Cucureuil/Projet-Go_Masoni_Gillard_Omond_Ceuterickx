96a4d62d6f365e889f271bad224f55199afd91f6
Lucille Gillard <lucille.gillard@macroprog.com>
2019-10-28 19:01:31 +0100

    Add: airport pressure data

--- IHM/src/app/modules/airport/airport.module.ts
+++ IHM/src/app/modules/airport/airport.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AirportPageComponent } from './components/airport-page/airport-page.component';
 import {AirportRouter} from './airport-routing.module';
+import {CommunModule} from '../commun/commun.module';
 
 @NgModule({
   declarations: [AirportPageComponent],
   imports: [
     CommonModule,
+    CommunModule,
     AirportRouter
   ]
 })
--- IHM/src/app/modules/airport/components/airport-page/airport-page.component.ts
+++ IHM/src/app/modules/airport/components/airport-page/airport-page.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {ActivatedRoute} from '@angular/router';
 import {ApiService} from '../../../commun/services/api.service';
+import {SensorModel} from "../../../commun/models/sensor.model";
 
 @Component({
   selector: 'app-airport-page',
@@ -8,7 +9,10 @@ import {ApiService} from '../../../commun/services/api.service';
   styleUrls: ['./airport-page.component.scss']
 })
 export class AirportPageComponent implements OnInit {
-  airportId : string;
+  airportId: string;
+  windDataList: SensorModel[] = [];
+  tempDataList: SensorModel[] = [];
+  pressDataList: SensorModel[] = [];
 
   constructor(private apiServ: ApiService, private route: ActivatedRoute) { }
 
@@ -20,8 +24,51 @@ export class AirportPageComponent implements OnInit {
   }
 
   private loadData() {
-    this.apiServ.doGetRequest(ApiService.API_GET_AIRPORT_DATA + this.airportId).subscribe( data => {
-      console.log(data);
+    this.apiServ.doGetRequest(ApiService.API_GET_AIRPORT_DATA + this.airportId).subscribe( res => {
+      const container = JSON.parse(JSON.stringify(res)).Data;
+      this.windDataList = [];
+      this.tempDataList = [];
+      this.pressDataList = [];
+
+      let data: any[] = container[0].Data;
+      if (data !== undefined && data != null) {
+        for (const sensor of data) {
+          this.windDataList.push(Object.assign(new SensorModel(), {
+            _idSensor: sensor.IdSensor,
+            _idAirport: sensor.IdAirport,
+            _value: sensor.Value,
+            _dateMeasure: sensor.DateMeasure
+          }));
+        }
+      }
+
+      data = container[1].Data;
+      if (data !== undefined && data != null) {
+        for (const sensor of data) {
+          this.tempDataList.push(Object.assign(new SensorModel(), {
+            _idSensor: sensor.IdSensor,
+            _idAirport: sensor.IdAirport,
+            _value: sensor.Value,
+            _dateMeasure: sensor.DateMeasure
+          }));
+        }
+      }
+
+      data = container[2].Data;
+      if (data !== undefined && data != null) {
+        for (const sensor of data) {
+          this.pressDataList.push(Object.assign(new SensorModel(), {
+            _idSensor: sensor.IdSensor,
+            _idAirport: sensor.IdAirport,
+            _value: sensor.Value,
+            _dateMeasure: sensor.DateMeasure
+          }));
+        }
+      }
     });
+  }
+
+  dateToString(date: Date) {
+    return date.toLocaleDateString() + ' ' + date.toLocaleTimeString();
   }
 }
